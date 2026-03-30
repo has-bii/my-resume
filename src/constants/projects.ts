10ee6b8c40dcfeaa20e5200f39f70dd0620c2169
has-bii <hasbii.rofficial@gmail.com>
2026-03-30 08:57:00 +0700

    fix: project images directory, remove '/public'

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -5,15 +5,15 @@ export const projects: Project[] = [
     name: 'Naratani Sales Mobile App',
     description:
       'A mobile application for sales teams to manage customers, track orders, and access product catalogs on the go. Included custom native library for printing via bluetooth.',
-    image: '/public/projects/naratani-sales.webp',
-    stack: ['Expo', 'Typescript', 'Custom Native Library'],
+    image: '/projects/naratani-sales.webp',
+    stack: ['Expo', 'Typescript', 'React Query', 'Better Auth', 'Custom Native Library'],
     year: 2026,
   },
   {
     name: 'Naratani Admin App',
     description:
       'A comprehensive admin dashboard for managing business operations, featuring real-time analytics, inventory management, and user administration.',
-    image: '/public/projects/naratani.webp',
+    image: '/projects/naratani.webp',
     stack: ['Typescript', 'Next.js', 'Tailwindcss', 'Nest.js', 'React Query', 'Axios', 'Prisma'],
     year: 2026,
   },
@@ -21,7 +21,7 @@ export const projects: Project[] = [
     name: 'Provent Mandiri Company Profile',
     description:
       "A modern, responsive company profile website showcasing Provent Mandiri's services and corporate identity with smooth animations.",
-    image: '/public/projects/provent-mandiri.webp',
+    image: '/projects/provent-mandiri.webp',
     stack: ['Typescript', 'Next.js', 'Framer Motion', 'Better Auth', 'R2:Cloudflare', 'Shadcn UI'],
     year: 2025,
   },
